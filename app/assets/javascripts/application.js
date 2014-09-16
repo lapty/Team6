f2751548fcad85de88d03825478661a43aae8a83
lapty <charles.qh.nguyen@gmail.com>
2014-09-16 12:05:12 -0400

    added gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular.min
+//= require angular-route.min
+//= require bootstrap.min
 //= require_tree .
---
+++ app/controllers/restuarants_controller.rb
@@ -0,0 +1,13 @@
+class RestuarantsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
