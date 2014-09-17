f1309aeb8349285a7107562bceb86a1ed9d17e99
Anna L. Ehrhardt <aehrhard@gmail.com>
2014-09-17 11:39:22 -0400

    item controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
  root 'welcome#index' 
   resources :restaurants do
-    resources :menu do
+    resources :menus do
+      resources :items do
+      end
     end
   end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
