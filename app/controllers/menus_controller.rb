a1c4a7c254fc0c7c3b3c1cd91cf6f78296b7566c
Michael Dion <michaeldion@Michaels-MacBook-Pro.local>
2014-09-17 10:59:45 -0400

    added json routes to restaurants controller and began menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,14 +1,28 @@
 class MenusController < ApplicationController
   before_action :authenticate_user
+  before_action :find_menu, only: [:show, :edit, :update, :destory]
   def index
+    @menus = Menu.all
+
   end
 
   def show
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @menu = @menu.restaurants.create menu_params
+    respond_to do |format|
+      format.json { render json: @menu.as_json}
+    end
   end
 
   def edit
   end
+
+  def update
+    @
 end
