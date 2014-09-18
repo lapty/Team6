a3b0a4c009950e72493376bfa69c6e8266191283
Will Gallop <wgallop99@gmail.com>
2014-09-18 19:53:07 -0400

    added edit item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @menu.item.update_attributes item_params
+    @item.update_attributes item_params
     respond_to do |format|
       format.json { render json: @item.as_json }
     end
