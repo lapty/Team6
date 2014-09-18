5cb0acd5c2523856b6bd6da0c93a0b40e515dafb
Will Gallop <wgallop99@gmail.com>
2014-09-18 14:38:56 -0400

    pulling

--- app/assets/javascripts/Controllers/controller.js
+++ app/assets/javascripts/Controllers/controller.js
@@ -12,65 +12,48 @@ angular.module("menuModule")
           scrollTop: $("#scroll").offset().top }, 750);  });
         });
 
-// CRUD FOR MENU
-
-        menuSvc.getMenus().then(function (menus) {
-          $log.info(menus);
-            $scope.menus = menus.data;
-        });
-
-        //  menuSvc.singleMenu($routeParams.id).then(function (response) {
-        //      $scope.singleMenu = response.data;
-        //  });
-
-        $scope.addMenu = function (menu) {
-            menuSvc.addMenu(menu);
-            $location.path("/admin/tacoboy");
-        };
-
 
 // // CRUD FOR MENU ITEMS
-//
-//        menuSvc.getItems().then(function (items) {
-//            $log.info(items);
-//            $scope.items = items.data;
-//
-//          });
-//
-//          menuSvc.singleItem($routeParams.id).then(function (response) {
-//              $scope.singleItem = response.data;
-//          });
-//
-//          $scope.addItem = function (item) {
-//              menuSvc.addItem(item);
-//
-//          };
-//
-//          $scope.editItem = function (item) {
-//              menuSvc.editItem(item);
-//          };
-//
-//          $scope.deleteItem = function (id) {
-//              menuSvc.deleteItem(id);
-//          };
+
+       menuSvc.getItems().then(function (items) {
+           $log.info(items);
+           $scope.items = items.data;
+
+         });
+
+        //  menuSvc.singleItem($routeParams.id).then(function (response) {
+        //      $scope.singleItem = response.data;
+        //  });
+
+         $scope.addItem = function (item) {
+             menuSvc.addItem(item);
+         };
+
+         $scope.editItem = function (item) {
+             menuSvc.editItem(item);
+         };
+
+         $scope.deleteItem = function (id) {
+             menuSvc.deleteItem(id);
+         };
 
 
-    $rootScope.$on("menu:deleted", function () {
-      menuSvc.getMenus().then(function (menus) {
-        $scope.menus = menus.data;
-        $route.reload();
-      });
-  });
-
-    $rootScope.$on("menu:added", function () {
-      menuSvc.getMenus().then(function (menus) {
-        $scope.menus = menus.data;
-      });
-  });
-
-    $rootScope.$on("item:added", function () {
-      menuSvc.getMsgs().then(function (items) {
-        $scope.msgs = msgs.data.reverse();
-    });
-  });
+  //   $rootScope.$on("menu:deleted", function () {
+  //     menuSvc.getMenus().then(function (menus) {
+  //       $scope.menus = menus.data;
+  //       $route.reload();
+  //     });
+  // });
+  //
+  //   $rootScope.$on("menu:added", function () {
+  //     menuSvc.getMenus().then(function (menus) {
+  //       $scope.menus = menus.data;
+  //     });
+  // });
+  //
+  //   $rootScope.$on("item:added", function () {
+  //     menuSvc.getMsgs().then(function (items) {
+  //       $scope.msgs = msgs.data.reverse();
+  //   });
+  // });
 });
--- app/assets/javascripts/Services/service.js
+++ app/assets/javascripts/Services/service.js
@@ -46,82 +46,47 @@ angular.module("menuModule")
           return rests;
         };
 
-        var menus1 = "/menus/1/items";
+// CRUD FOR MENU ITEMS
 
-        var getMenus = function(){
-          return $http.get(menus);
+        var items = "/menus/1/items";
+
+        var getItems = function(){
+          return $http.get(items);
         };
 
-        var createMenu = function(menu) {
-          $http.post(menus, menu).then(function (response) {
-                $rootScope.$broadcast("menu:added");
-                $log.info("menu:added");
+        // var singleItem = function(id) {
+        //    return $http.get(items + "/" + id);
+        // };
+
+        var createItem = function(item) {
+          return $http.post(items, item).then(function (response) {
+                $rootScope.$broadcast("item:added");
+                $log.info("item:added");
             })
         };
 
-        var deleteMenu = function(menu) {
-          return $http.delete(menus + "/" + menu.id, menu).then(function (response) {
+        var deleteItem = function(item) {
+          return $http.delete(items + "/" + item._id, item).then(function (response) {
                 console.log(response);
-                $rootScope.$broadcast("menu:deleted");
-                $log.info("menu:deleted");
+                $rootScope.$broadcast("item:deleted");
+                $log.info("item:deleted");
             })
         };
 
-        var editMenu = function(menu) {
-          return $http.put(menus + "/" + menu.id, menu).then(function (response) {
-                $rootScope.$broadcast("menu:updated");
-                $log.info("menu:updated");
+        var editItem = function(item) {
+          return $http.put(items + "/" + item._id, item).then(function (response) {
+                $rootScope.$broadcast("item:updated");
+                $log.info("item:updated");
             })
         };
-
-
-// // CRUD FOR MENU ITEMS
-//
-//         var items = "/menus/:menu_id/items";
-//
-//         var getItems = function(){
-//           return $http.get(items);
-//         };
-//
-//         var singleItem = function(id) {
-//            return $http.get(items + "/" + id);
-//         };
-//
-//         var createItem = function(item) {
-//           return $http.post(items, item).then(function (response) {
-//                 $rootScope.$broadcast("item:added");
-//                 $log.info("item:added");
-//             })
-//         };
-//
-//         var deleteItem = function(item) {
-//           return $http.delete(items + "/" + item._id, item).then(function (response) {
-//                 console.log(response);
-//                 $rootScope.$broadcast("item:deleted");
-//                 $log.info("item:deleted");
-//             })
-//         };
-//
-//         var editItem = function(item) {
-//           return $http.put(items + "/" + item._id, item).then(function (response) {
-//                 $rootScope.$broadcast("item:updated");
-//                 $log.info("item:updated");
-//             })
-//         };
 
         return {
           getRests: getRests,
-          // Menus
-          getMenus: getMenus,
-          // singleMenu: singleMenu,
-          addMenu: createMenu,
-          deleteMenu: deleteMenu,
-          editMenu: editMenu
           // // Menu Items
-          // getItems: getItems,
+          getItems: getItems,
           // singleItem: singleItem,
-          // addItem: createItem,
-          // deleteItem: deleteItem,
-          // editItem: editItem,
+          addItem: createItem,
+          deleteItem: deleteItem,
+          editItem: editItem,
         };
     });
